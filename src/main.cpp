9acc79a7dfe3eaae6d0079c940384adbd2061659
Flo <flocela@gmail.com>
2021-05-09 17:48:57 +0000

    show a bmp image.

--- src/main.cpp
+++ src/main.cpp
@@ -6,74 +6,43 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-bool init();
-bool loadMedia();
-void close();
-SDL_Window* gWindow = NULL;
-SDL_Surface* gScreenSurface = NULL;
-SDL_Surface* gHelloWorld = NULL;
-
-bool init()
-{
-	bool success = true;
-	if ( SDL_Init( SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-		success = false;
-	}
-	else
-	{
-		gScreenSurface = SDL_GetWindowSurface (gWindow);
-	}
-	return success;
-}
-
-bool loadMedia()
-{
-	bool success = true;
-	gHelloWorld = SDL_LoadBMP("./hello_world.bmp");
-	if(gHelloWorld == NULL)
-	{
-		printf("Unable to load image %s! SDL Error: %s\n",
-				"hello_world.bmp", SDL_GetError());
-		success =false;
-	}
-	return success;
-}
-
-void close()
-{
-	SDL_FreeSurface( gHelloWorld);
-	gHelloWorld = NULL;
-	SDL_DestroyWindow( gWindow);
-	gWindow = NULL;
-	SDL_Quit;
-}
-
 int main(int argc, char* args[]) {
-	if(!init())
-	{
-		printf("Failed to initialize\n");
-	}
-	else
-	{
-		if (!loadMedia())
-		{
-			printf("Failed to load media!\n");
-		}
-		else
-		{
-			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
-			SDL_UpdateWindowSurface(gWindow);
-			SDL_Delay(2000);
-		}
-		close();
-		return 0;
-	}
-	
 
+	bool quit = false;
+	SDL_Event event;
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window *window = SDL_CreateWindow(
+		"SDL2 Displaying Image",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		640,
+		480,
+		0);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_Surface* image = SDL_LoadBMP("../images/hello_world.bmp");
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, image);
 	
-  
+	
+	while (!quit)
+	{
+		SDL_WaitEvent(&event);
+		switch(event.type)
+		{
+			case SDL_QUIT:
+				quit = true;
+				break;
+		}
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyTexture(texture);
+	SDL_FreeSurface(image);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
   /*
   constexpr std::size_t kFramesPerSecond{60};
   constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
