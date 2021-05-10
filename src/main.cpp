5ed79aad90ff19eb44524eaf3e81766a4d113744
Flo <flocela@gmail.com>
2021-05-10 01:27:01 +0000

    changed main back to original

--- src/main.cpp
+++ src/main.cpp
@@ -8,42 +8,6 @@ const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char* args[]) {
 
-	bool quit = false;
-	SDL_Event event;
-	SDL_Init(SDL_INIT_VIDEO);
-
-	SDL_Window *window = SDL_CreateWindow(
-		"SDL2 Displaying Image",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		640,
-		480,
-		0);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-	SDL_Surface* image = SDL_LoadBMP("../images/hello_world.bmp");
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, image);
-	
-	
-	while (!quit)
-	{
-		SDL_WaitEvent(&event);
-		switch(event.type)
-		{
-			case SDL_QUIT:
-				quit = true;
-				break;
-		}
-		SDL_RenderCopy(renderer, texture, NULL, NULL);
-		SDL_RenderPresent(renderer);
-	}
-
-	SDL_DestroyTexture(texture);
-	SDL_FreeSurface(image);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
-  /*
   constexpr std::size_t kFramesPerSecond{60};
   constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
   constexpr std::size_t kScreenWidth{640};
@@ -57,7 +21,7 @@ int main(int argc, char* args[]) {
   game.Run(controller, renderer, kMsPerFrame);
   std::cout << "Game has terminated successfully!\n";
   std::cout << "Score: " << game.GetScore() << "\n";
-  std::cout << "Size: " << game.GetSize() << "\n"; */
+  std::cout << "Size: " << game.GetSize() << "\n"; 
 
   return 0;
 }
